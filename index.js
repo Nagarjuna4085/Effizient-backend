5103cb15204de668bee430788ae27c3c13047195
Nagarjuna4085 <nagarjuna.sanem@fmail.com>
2023-09-01 01:23:44 +0530

    still checking

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 //get Data
 
 app.post('/hi',async(req,res)=>{
+  console.log(req)
   const dbUrl = "mongodb+srv://nagarjuna:sanam123@cluster0.oynhj.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(dbUrl,  {
   serverApi: {
