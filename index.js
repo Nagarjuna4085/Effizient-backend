a7b64f6c02e760f4e69110be299998e42e4f3b00
Nagarjuna4085 <nagarjuna.sanem@fmail.com>
2023-09-01 01:13:14 +0530

    method changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 //get Data
 
-app.get('/hi',async(req,res)=>{
+app.post('/hi',async(req,res)=>{
   const dbUrl = "mongodb+srv://nagarjuna:sanam123@cluster0.oynhj.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(dbUrl,  {
   serverApi: {
