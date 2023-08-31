c84d15920a9499c6b8470a280c98236db1fb2100
Nagarjuna4085 <nagarjuna.sanem@fmail.com>
2023-09-01 00:54:25 +0530

    testing deployment in on render app

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // mongodb+srv://nagarjuna:sanam@cluster0.oynhj.mongodb.net/?retryWrites=true&w=majority
 import express from 'express'
 import cors from 'cors'
+import MongoClientInstance from './conn.js';
+const  MongoDBClient = new MongoClientInstance()
 const port = process.env.PORT || 3010;
+const database = MongoDBClient._client.db("Effizient");
+const lor = database.collection("lor");
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -10,6 +14,24 @@ app.use(express.json())
 app.get('/hi',(req,res)=>{
   console.log(req)
   res.send({name:"nagarjuna"})
+
+})
+app.get('/getFormData', async ({req, res}) => {
+  console.log(req)
+  try {
+    await MongoDBClient._client.connect()
+    console.log("inserting")
+    await lor.insertOne({
+      title: "Record of a Shriveled Datum",
+      content: "No bytes, no problem. Just insert a document, in MongoDB",
+    })
+    console.log("inserted")
+    return
+  
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+
+  }
 
 })
 app.listen(port, '0.0.0.0', () => {
