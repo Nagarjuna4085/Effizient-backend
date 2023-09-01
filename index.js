46f1db225497a151cb512e5c8515cc9f3654b4a6
Nagarjuna4085 <nagarjuna.sanem@fmail.com>
2023-09-01 21:07:15 +0530

    removed unwanted files

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ import * as dotenv from "dotenv";
 import nodemailer from "nodemailer"
 import PDFDocument from 'pdfkit'
 import fs from 'fs'
-const doc = new PDFDocument(
-  {
-    size: 'A4'
-}
 
-);
 
 dotenv.config();
 import mongoose from 'mongoose';
@@ -25,42 +20,42 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 25,
   auth: {
-      user: 'nagarjuna.sanem@gmail.com',
-      pass: 'oloqdomyfgcrtjoj'
-      
+    user: 'nagarjuna.sanem@gmail.com',
+    pass: 'oloqdomyfgcrtjoj'
+
   },
   //  sendmail: true 
 });
-async function main(filename,to,text) {
+async function main(filename, to, text) {
   // send mail with defined transport object
   let mail = {
     from: "nagarjuna.sanem@gmail.com",
     to: to,
     subject: "Statement of Purpose",
     text: text,
-    attachments:[
-        {   // binary buffer as an attachment
-            filename: filename,
-            content: fs.createReadStream(filename)
+    attachments: [
+      {   // binary buffer as an attachment
+        filename: filename,
+        content: fs.createReadStream(filename)
 
-        },
+      },
     ]
-};
- 
-transporter.sendMail(mail, function (error, info) {
+  };
+
+  transporter.sendMail(mail, function (error, info) {
     if (error) {
-        console.log(error);
-        fs.rmSync(filename, {
-            force: true,
-        });
+      console.log(error);
+      fs.rmSync(filename, {
+        force: true,
+      });
     } else {
-        console.log("Email sent successfully: "
-            + info.response);
-            fs.rmSync(filename, {
-                force: true,
-            });
+      console.log("Email sent successfully: "
+        + info.response);
+      fs.rmSync(filename, {
+        force: true,
+      });
     }
-});
+  });
 }
 const openai = new OpenAIApi(configration);
 
@@ -70,24 +65,78 @@ app.use(cors())
 app.use(express.json())
 // mongodb+srv://nagarjuna:sanam123@cluster0.oynhj.mongodb.net/?retryWrites=true&w=majority
 const dbUrl = "mongodb+srv://nagarjuna:sanam123@cluster0.oynhj.mongodb.net/Effizient1?retryWrites=true&w=majority";
- const connectionParams = {
+const connectionParams = {
   useNewUrlParser: true, useUnifiedTopology: true
 };
 await mongoose.connect(dbUrl, connectionParams)
 sop.watch().
-  on('change', async(data) => {
-    // Math.floor(Math.random() * 100000000000)
+  on('change', async (data) => {
+    const doc = new PDFDocument(
+      {
+        size: 'A4'
+      }
     
-    const object =data.fullDocument
-    console.log(data.fullDocument)
-    var filename = object.fullName+'.pdf'
+    );
+    // Math.floor(Math.random() * 100000000000)
+
+    const object = data.fullDocument
+    // console.log(data.fullDocument)
+    var filename = object.fullName + '.pdf'
     doc.pipe(fs.createWriteStream(filename));
+    doc.fontSize(18);
+    doc.font('Times-Roman').text(`From,`, {
+      align: 'left'
+    }
+    ); doc.font('Times-Roman').text(`${object.fullName}`, {
+
+      align: 'left'
+    }
+    ); doc.font('Times-Roman').text(`1-38,Enakandla`, {
+
+      align: 'left'
+    }
+    ); doc.font('Times-Roman').text(`${object.email}`, {
+
+      align: 'left'
+    }
+    );
+    doc.moveDown();
+    doc.font('Times-Roman').text(`To`, {
+
+      align: 'left'
+    }
+    ); doc.font('Times-Roman').text(`Visa Officer`, {
+
+      align: 'left'
+    }
+    ); doc.font('Times-Roman').text(`High Commission of Canada`, {
+
+      align: 'left'
+    }
+    ); doc.font('Times-Roman').text(`London, United Kingdom`, {
+
+      align: 'left'
+    }
+    );
+
+    doc.moveDown();
+    doc.font('Times-Roman').text(`Subject: Statement of Purpose for studying in Canada`, {
+
+      align: 'left'
+    }
+    );
+    doc.moveDown();
+    doc.font('Times-Roman').text(`Dear Sir/Madam,`, {
+
+      align: 'left'
+    }
+    );
 
     // console.log(data.documentKey._id)
     // var data =sop.findById(data.documentKey._id)
     // console.log(data)
     try {
-      const question =`Could you please assist me in creating a Statement of Purpose (SOP) with 5000 words? I have the following details that I'd like to include: my email is ${object.email}, full name is ${object.fullName}, age is ${object.age}, highest level of education is ${object.highestEducation},${object.graduationCollege} is the institute where I completed my highest level of education, ${object.stream} field of study,I have ${object.experience} relevant work experience, ${object.admittedUniversityName} institute I got admitted to in Canada, ${object.programName} program of study in Canada, country I'm applying from ${object.yourCountryName},${object.futureGoals} are my future goals, English language proficiency scores (${object.listeningScore}, ${object.readingScore}, ${object.speakingScore}, ${object.writingScore}), first-year tuition payment status is ${object.isTutionFeePaid}, tuition fee amount paid ${object.feePaidValue}, and whether I participated in the Guaranteed Investment Certificate (GIC) program is ${object.isGic} and paid GIC is ${object.paidGicValue}. Could you help me craft a well-structured SOP using these details?`
+      const question = `Could you please assist me in creating a Statement of Purpose (SOP) with 5000 words? I have the following details that I'd like to include: my email is ${object.email}, full name is ${object.fullName}, age is ${object.age}, highest level of education is ${object.highestEducation},${object.graduationCollege} is the institute where I completed my highest level of education, ${object.stream} field of study,I have ${object.experience} relevant work experience, ${object.admittedUniversityName} institute I got admitted to in Canada, ${object.programName} program of study in Canada, country I'm applying from ${object.yourCountryName},${object.futureGoals} are my future goals, English language proficiency scores (${object.listeningScore}, ${object.readingScore}, ${object.speakingScore}, ${object.writingScore}), first-year tuition payment status is ${object.isTutionFeePaid}, tuition fee amount paid ${object.feePaidValue}, and whether I participated in the Guaranteed Investment Certificate (GIC) program is ${object.isGic} and paid GIC is ${object.paidGicValue}. Could you help me craft a well-structured SOP using these details?`
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: question,
@@ -97,98 +146,47 @@ sop.watch().
         frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
         presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
       });
-    console.log(response.data.choices[0].text)
-    doc.fontSize(18);
-doc.font('Times-Roman').text(`From.`, {
+      // console.log(response.data.choices[0].text)
+      const ai = response.data.choices[0].text
+      console.log(ai)
 
-  align: 'left'
-}
-);doc.font('Times-Roman').text(`${object.fullName}`, {
+      doc.fontSize(18).font('Times-Roman').text(ai);
+      doc.moveDown();
 
-  align: 'left'
-}
-);doc.font('Times-Roman').text(`1-38,Enakandla`, {
+      doc.font('Times-Roman').text(`Sincerely,`, {
 
-  align: 'left'
-}
-);doc.font('Times-Roman').text(`${object.email}`, {
+        align: 'left'
+      }
+      );
+      doc.moveDown();
+      doc.font('Times-Roman').text(`${object.fullName}`, {
 
-  align: 'left'
-}
-);
-doc.moveDown();
-doc.font('Times-Roman').text(`To`, {
+        align: 'left'
+      }
+      );
+      doc.end();
+      var text = `Dear ${object.fullName} Please find the Statement of Purpose template for your student visa application to Canada. kingly edit it as per your scenario and needs.`
+      await main(filename, object.email, text)
 
-  align: 'left'
-}
-);doc.font('Times-Roman').text(`Visa Officer`, {
-
-  align: 'left'
-}
-);doc.font('Times-Roman').text(`High Commission of Canada`, {
-
-  align: 'left'
-}
-);doc.font('Times-Roman').text(`London, United Kingdom`, {
-
-  align: 'left'
-}
-);
-
-doc.moveDown();
-doc.font('Times-Roman').text(`Subject: Statement of Purpose for studying in Canada`, {
-
-  align: 'left'
-}
-);
-doc.moveDown();
-doc.font('Times-Roman').text(`Dear Sir/Madam,`, {
-
-  align: 'left'
-}
-);
-
-  
-    
-    doc
-  .fontSize(18).font('Times-Roman')
-  .text(response.data.choices[0].text);
-doc.moveDown();
-
-  doc.font('Times-Roman').text(`Sincerely,`, {
-
-    align: 'left'
-  }
-  );
-doc.moveDown();
-doc.font('Times-Roman').text(`${object.fullName}`, {
-
-  align: 'left'
-}
-);
-  doc.end();
-  var text = `Dear ${object.fullName} Please find the Statement of Purpose template for your student visa application to Canada. kingly edit it as per your scenario and needs.`
- await  main(filename,object.email,text)
-      
     } catch (error) {
-      console.log("error",error)
+      console.log("error", error)
 
-   
+
     }
   });
-    
-   
-  // });
 
-app.post('/sendformdata',async(req,res)=>{
+
+// });
+
+app.post('/sendformdata', async (req, res) => {
   const data = req.body.data
-   
+
   // const Person = mongoose.model('lor', new mongoose.Schema({ title: String,content:String }));
   const Person = new sop(data);
-  
+
   await Person.save()
 
-  res.send({name:"nagarjuna",body:req.body})
+  res.send({ status: "success", })
 
 })
 
